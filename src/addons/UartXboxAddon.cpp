9a478004395dd62733bba1cb5acc1545578cee6d
victormantuaneli <victormantuaneli@yahoo.com.br>
2025-04-14 17:35:56 -0300

    Update UartXboxAddon.cpp

--- src/addons/UartXboxAddon.cpp
+++ src/addons/UartXboxAddon.cpp
@@ -124,3 +124,7 @@ uint16_t UartXboxInput::getJoystickXValue() {
 uint16_t UartXboxInput::getJoystickYValue() {
     return 0;  // Substitua com a leitura real do joystick
 }
+
+uint16_t UartXboxInput::map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
