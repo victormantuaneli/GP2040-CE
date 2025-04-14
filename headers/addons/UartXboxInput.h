1aec3e98ad67117b5851e7fd0836493aa05c66f0
victormantuaneli <victormantuaneli@yahoo.com.br>
2025-04-14 17:00:00 -0300

    Update UartXboxInput.h

--- headers/addons/UartXboxInput.h
+++ headers/addons/UartXboxInput.h
@@ -3,19 +3,25 @@
 
 #include <stdint.h>
 #include "addons/gpaddon.h"
-#include "gpaddon.h"
-#include "gamepad.h"
+
+#define UART_ID uart1            // Pode ser uart0 se preferir
+#define UART_BAUDRATE 9600      // Ajuste conforme o dispositivo
+#define UART_TX_PIN 4           // Ajuste conforme necessário
+#define UART_RX_PIN 5           // Ajuste conforme necessário
 
 class UartXboxInput : public GPAddon {
 public:
-    virtual bool available();
-    virtual void setup();       // SNESpad Setup
-    virtual void process();     // SNESpad Process
+    virtual bool available();       // Verifica se deve ativar o addon
+    virtual void setup();           // Configura a UART
+    virtual void process();         // Lê dados UART e atualiza Gamepad
     virtual void preprocess() {}
     virtual void postprocess(bool sent) {}
     virtual void reinit() {}
-private:
 
+private:
+    uint32_t nextPollMs = 0;
+    bool buttonA = false;
+    bool buttonB = false;
 };
 
 #endif // _UART_XBOX_INPUT_H_
